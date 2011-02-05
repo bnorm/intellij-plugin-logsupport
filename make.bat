ace6cdfe4e24f1506e3baeab2cc08958634c220b
jkellerer <jkellerer@localhost>
2011-02-05 13:50:20 +0000

    Fixed path issues.

--- make.bat
+++ make.bat
@@ -1,17 +1,10 @@
 @echo off
 SETLOCAL
-SET JDK32=C:\Projects\DevEnv\jdk32
 
-IF EXIST %JDK32% (
-	echo.
-	echo Ensuring build is 32bit, using JDK %JDK32%
-	SET JAVA_HOME=%JDK32%
-)
-
+IF NOT EXIST "%ProgramFiles(x86)%" SET ProgramFiles(x86)=%ProgramFiles%
 IF NOT EXIST "%ProgramFiles%\JetBrains" (
 	echo.
 	echo Warning: "%ProgramFiles%\JetBrains" not found, build may break!
-	echo Make sure build is running in 32bit mode to fix path issues.
 	echo.
 )
 
