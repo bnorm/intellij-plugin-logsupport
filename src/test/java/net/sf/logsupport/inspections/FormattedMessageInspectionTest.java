ac1a689f26184cd7ed20338a74afb9879de3fb8d
jkellerer <jkellerer@localhost>
2011-05-17 10:59:27 +0000

    Handled fatal errors in IDEA 10.5 and 10.0.2.

---
+++ src/test/java/net/sf/logsupport/inspections/FormattedMessageInspectionTest.java
@@ -0,0 +1,28 @@
+package net.sf.logsupport.inspections;
+
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.PsiTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * TODO: Create Description.
+ *
+ * @author Juergen_Kellerer, 2011-02-13
+ * @version 1.0
+ */
+public class FormattedMessageInspectionTest extends PsiTestCase {
+
+	private static final String TEST_CODE = "public class Test { Test() { int i = 1; } }";
+	PsiFile file;
+
+	@Before
+	public void initFile() throws Exception {
+		file = createFile("Test.java", TEST_CODE);
+	}
+
+	@Test
+	public void testDummy() {
+		file.getFileType();
+	}
+}
