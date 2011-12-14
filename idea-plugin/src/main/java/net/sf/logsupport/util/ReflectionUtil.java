7501eebe7cf620f3ede595f2a037c6dd38613370
Juergen Kellerer <Juergen Kellerer@localhost>
2011-12-14 19:01:08 +0100

    Enhanced the reflection util to support private methods when invoking them using reflection.

--- idea-plugin/src/main/java/net/sf/logsupport/util/ReflectionUtil.java
+++ idea-plugin/src/main/java/net/sf/logsupport/util/ReflectionUtil.java
@@ -34,7 +34,7 @@ public class ReflectionUtil {
 	/**
 	 * Returns the field value using reflection.
 	 *
-	 * @param cls	   the class to get the field from.
+	 * @param cls       the class to get the field from.
 	 * @param instance  the instance to get the field from (use 'null' to access static fields).
 	 * @param fieldName the name of the field.
 	 * @param <T>       the type of the value.
@@ -55,7 +55,7 @@ public class ReflectionUtil {
 	 *
 	 * @param instance   the instance to invoke the method on.
 	 * @param methodName the name of the method to invoke.
-	 * @param params	 the params to pass to the method.
+	 * @param params     the params to pass to the method.
 	 * @param <T>        the type of the return value.
 	 * @return the return value of the method or 'null' if the method signature is void.
 	 */
@@ -70,13 +70,13 @@ public class ReflectionUtil {
 	 * @param instance   the instance to invoke the method on.
 	 * @param methodName the name of the method to invoke.
 	 * @param paramTypes the types of the params (only needed if one of the params is 'null').
-	 * @param params	 the params to pass to the method.
+	 * @param params     the params to pass to the method.
 	 * @param <T>        the type of the return value.
 	 * @return the return value of the method or 'null' if the method signature is void.
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T invoke(Object instance, String methodName,
-							   @Nullable Class<?>[] paramTypes, Object... params) {
+	                           @Nullable Class<?>[] paramTypes, Object... params) {
 		if (paramTypes == null) {
 			paramTypes = new Class[params.length];
 			for (int i = 0; i < paramTypes.length; i++) {
@@ -105,34 +105,49 @@ public class ReflectionUtil {
 		final List<Object> key = new ArrayList<Object>();
 		key.add(cls);
 		key.add(methodName);
-		for (Class type : paramTypes) key.add(type);
+		Collections.addAll(key, paramTypes);
 
 		Method method = methods.get(key);
 		if (method == null) {
-			search:
-			for (Method m : cls.getMethods()) {
-				if (!m.getName().equals(methodName))
-					continue;
+			Method[] methodList = cls.getMethods();
 
-				Class<?>[] mParamTypes = m.getParameterTypes();
-				if (mParamTypes.length != paramTypes.length)
-					continue;
+			// Look after a public method that matches the signature
+			method = doFindMethod(methodName, methodList, paramTypes);
 
-				for (int i = 0; i < mParamTypes.length; i++)
-					if (!mParamTypes[i].isAssignableFrom(paramTypes[i]))
-						continue search;
-
-				method = m;
-				break;
+			// Look after in-accessible methods if public methods didn't match.
+			for (Class<?> c = cls; method == null && c != null; c = c.getSuperclass()) {
+				methodList = c.getDeclaredMethods();
+				method = doFindMethod(methodName, methodList, paramTypes);
 			}
 
 			if (method == null)
 				throw new NoSuchMethodError(cls.getName() + "." + methodName + "(" + Arrays.asList(paramTypes) + ")");
+			else if (!method.isAccessible())
+				method.setAccessible(true);
 
 			methods.put(key, method);
 		}
 
 		return method;
+	}
+
+	private static Method doFindMethod(String methodName, Method[] methods, Class[] paramTypes) {
+		search:
+		for (Method method : methods) {
+			if (!method.getName().equals(methodName))
+				continue;
+
+			Class<?>[] mParamTypes = method.getParameterTypes();
+			if (mParamTypes.length != paramTypes.length)
+				continue;
+
+			for (int i = 0; i < mParamTypes.length; i++)
+				if (!mParamTypes[i].isAssignableFrom(paramTypes[i]))
+					continue search;
+
+			return method;
+		}
+		return null;
 	}
 
 	private ReflectionUtil() {
