2ffc19ea660059c18fca2614b4de4e19cc8b1fb7
Juergen Kellerer <Juergen Kellerer@localhost>
2011-12-14 17:37:50 +0100

    adjusted sample project and project files

--- test-sample/src/main/java/net/sf/logsupport/android/DroidLogger.java
+++ test-sample/src/main/java/net/sf/logsupport/android/DroidLogger.java
@@ -9,6 +9,10 @@ public class DroidLogger {
 
         if (Log.isLoggable(MY_TAG, Log.DEBUG)) { Log.d(MY_TAG, "fsdfsdf"); }
 
+        if (Log.isLoggable("MyTag", Log.DEBUG)) {
+            Log.d("MyTag", "sadas");
+        }
+
         Log.i(MY_TAG, "asdsadasd");
         boolean verbose = Log.isLoggable(MY_TAG, Log.VERBOSE);
         if (verbose) { Log.v(MY_TAG, "asdasdsadasdsd"); }
